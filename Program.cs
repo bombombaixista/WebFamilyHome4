594e665655d6f76d1d330e4c2db9585ed9f19189
bombombaixista <seuemail@exemplo.com>
2025-12-10 06:26:38 -0300

    Adiciona Dockerfile e ajustes no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using WebFamilyHome.Data;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// PostgreSQL
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Adiciona suporte a Controllers + Views (MVC)
+﻿var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Middleware padrão
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.UseRouting();
-app.UseAuthorization();
 
-// Rota padrão MVC
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
